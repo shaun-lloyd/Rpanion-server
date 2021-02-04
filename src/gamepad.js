f4751ab05f590504ac9a8531a6ade7562cd9a7a4
Shaun Lloyd <shaun@lloydie.com.au>
2021-02-04 11:11:34 +1100

    FMT TABS OMG

--- src/gamepad.js
+++ src/gamepad.js
@@ -47,7 +47,7 @@ state:
 	controller			defined controller array
 	eventLoop			gamepadIO event loop.
 	eventLoopRate		update rate in milliseconds
-
+}
 controller[index].
 	id					As recv from gamepad.id
 	name				User defined 'human' title
@@ -62,163 +62,155 @@ controller[index].
 */
 
 function toFixedDec(num){
-	var pow = Math.pow(10||10, 0)
-	return Math.round(num*pow) / pow
+  var pow = Math.pow(10||10, 0)
+  return Math.round(num*pow) / pow
 } 
 
 export default class Gamepad extends basePage {
-	constructor(props) {
-		super(props)
-		this.state = {
-			gamepad: [],		
-			controller:	[],
-			eventLoopRate: 1000,
-			loading: false,
-			waiting: false,
-			error: null,
-			infoMessage: null
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      gamepad: [],		
+      controller:	[],
+      eventLoopRate: 1000,
+      loading: false,
+      waiting: false,
+      error: null,
+      infoMessage: null
+    }
+  }
 
-	gamepadConnect = (event) => {
-		//	BUG		verify controller not in buffer, chrome/brave seem ok but firefox hmmm.
-		this.setState({
-			gamepad: [ ...this.state.gamepad, event.gamepad ]
-		})
-		//	YAH		this.gamepadControllerLoad(event.gamepad)
-		this.gamepadConfigCreate(event.gamepad)
-		this.gamepadEventLoop(this.state.gamepad[0])
-		console.log("Gamepad Connected: ", event.gamepad)
-	}    
+  gamepadConnect = (event) => {
+    //	BUG		verify controller not in buffer, chrome/brave seem ok but firefox hmmm.
+    this.setState({
+      gamepad: [ ...this.state.gamepad, event.gamepad ]
+    })
+    //	YAH		this.gamepadControllerLoad(event.gamepad)
+    this.gamepadConfigCreate(event.gamepad)
+    this.gamepadEventLoop(this.state.gamepad[0])
+    console.log("Gamepad Connected: ", event.gamepad)
+  }    
 
-	gamepadDisconnect = (event) => {
-		clearInterval(this.state.eventLoop)
-		const gamepad = this.state.gamepad.filter(gamepad => gamepad.id !== event.gamepad.id)
-		this.setState({
-			gamepad: gamepad 
-		})
-		console.log("Gamepad Disconnect: ", event.gamepad.id)
-	}
+  gamepadDisconnect = (event) => {
+    clearInterval(this.state.eventLoop)
+    const gamepad = this.state.gamepad.filter(gamepad => gamepad.id !== event.gamepad.id)
+    this.setState({
+      gamepad: gamepad 
+    })
+    console.log("Gamepad Disconnect: ", event.gamepad.id)
+  }
 
-	gamepadEventLoop = (gamepad) => {
-		this.state.eventLoop = setInterval(() => (this.gamepadIO(gamepad)), this.state.eventLoopRate)
-		console.log('Gamepad Streaming.')
-	}
+  gamepadEventLoop = (gamepad) => {
+    this.state.eventLoop = setInterval(() => (this.gamepadIO(gamepad)), this.state.eventLoopRate)
+    console.log('Gamepad Streaming.')
+  }
 
-	gamepadIO = (gamepad) => {
-		var i = 0
-		var _debug = ''
-		for(i=0; i < gamepad.axes.length; i++) {
-		var v = toFixedDec(gamepad.axes[i] * 1000)
-		this.state.controller[0].channel[i].value = v
-			_debug += ' '+v.toFixed()
-		}
-		//	YAH		this.gamepadSend(x,y,z,r)
-		console.log('Debug: ', _debug)
-		console.log('Controller: ', this.state.controller[0].channel)
-	}
+  gamepadIO = (gamepad) => {
+    var i = 0
+    var _debug = ''
+    for(i=0; i < gamepad.axes.length; i++) {
+      var v = toFixedDec(gamepad.axes[i] * 1000)
+      this.state.controller[0].channel[i].value = v
+      _debug += ' '+v.toFixed()
+    }
+    //	YAH		this.gamepadSend(x,y,z,r)
+    console.log('Debug: ', _debug)
+    console.log('Controller: ', this.state.controller[0].channel)
+  }
 
-	gamepadConfigGet = (controller) => {
-	}
+  gamepadConfigGet = (controller) => {
+  }
 
-	gamepadConfigSet = (controller) => {
-	}
+  gamepadConfigSet = (controller) => {
+  }
 
-	gamepadConfigCreate = (gamepad) => {
-		var controller = {
-			id: gamepad.id,
-			name: '',
-			aptarget: null,
-			channel: []
-		}
-		this.setState({
-			controller: [...this.state.controller, controller]
-		})
-		var i = 0
-		for(i=0; i < gamepad.axes.length; i++) {
-			var channel = {
-				name: '',
-				src: '', 
-				weight: 100,
-				min: -1000,
-				max: 1000
-			}
-			this.state.controller[0].channel = [...this.state.controller[0].channel, channel]
-		}
-		console.log("GamepadConfigCreated: ", this.state.controller[0])
-	}
+  gamepadConfigCreate = (gamepad) => {
+    var controller = {
+      id: gamepad.id,
+      name: '',
+      aptarget: null,
+      channel: []
+    }
+    this.setState({
+      controller: [...this.state.controller, controller]
+    })
+    var i = 0
+    for(i=0; i < gamepad.axes.length; i++) {
+      var channel = {
+	name: '',
+	src: '', 
+	weight: 100,
+	min: -1000,
+	max: 1000
+      }
+      this.state.controller[0].channel = [...this.state.controller[0].channel, channel]
+    }
+    console.log("GamepadConfigCreated: ", this.state.controller[0])
+  }
 
-	getMavMode() {
+  setMavManualControl = (x,y,z,r) => {
+    console.log('test')
+  }
 
-	}
+  componentDidMount() {
+    window.addEventListener("gamepadconnected", this.gamepadConnect)
+    window.addEventListener("gamepaddisconnected", this.gamepadDisconnect)
+    this.loadDone()
+  }
 
-	setMavMode(mode) {
+  componentWillUnmount() {
+    clearTimeout(this.state.eventLoop)
+    this.gamepadDisconnect()
+  }
 
-	}
+  renderTitle() {
+    return "Gamepad"
+  }
 
-	setMavManualControl(x,y,z,r) {
+  renderControllerChannelData(controller) {
+    console.log('Controller: ', typeof controller)
+    if(typeof controller !== 'undefined') {
+      return controller.channel.map((output, index) => {
+	return (
+	  <tr key={index}>
+	  <td>{(index + 1)}</td>
+	  <td>{output.name}</td>
+	  <td>{output.src}</td>
+	  <td>{output.weight}</td>
+	  <td>{output.min}</td>
+	  <td>{output.max}</td>
+	  <td>{output.val}</td>
+	  </tr>
+	)
+      })
+    }
+  }
 
-	}
+  // renderContent() {
+  // return (
+  // <div>
+  // <div style={{ display: (this.state.controller[0].id !== null) ? "block" : "none"}}>
+  // <p>Controller channel assignment and configuration</p>
+  // <Table id='gamepad-controller' striped bordered hover size="sm">
+  // <thead>
+  // <tr><th>Channel</th><th>Name</th><th>Source</th><th>Weight</th><th>Min</th><th>Max</th><th>Value</th></tr>
+  // {this.renderControllerChannelData(this.state.controller)}
+  // </thead>
+  // <tbody>
+  // </tbody>
+  // </Table>
+  // </div>
+  // <div style={{ display: (this.state.controller[0].id === null) ? "block" : "none"}}>
+  // <p>Please move an axes on the controller, to connect.</p>
+  // </div>
+  // </div>
+  // )
+  // }
 
-	componentDidMount() {
-		window.addEventListener("gamepadconnected", this.gamepadConnect)
-		window.addEventListener("gamepaddisconnected", this.gamepadDisconnect)
-		this.loadDone()
-	}
-
-	componentWillUnmount() {
-		clearTimeout(this.state.eventLoop)
-		this.gamepadDisconnect()
-	}
-
-	renderTitle() {
-		return "Gamepad"
-	}
-
-	renderControllerChannelData(controller) {
-		console.log('Controller: ', typeof controller)
-		if(typeof controller !== 'undefined') {
-		return controller.channel.map((output, index) => {
-			return (
-				<tr key={index}>
-				<td>{(index + 1)}</td>
-				<td>{output.name}</td>
-				<td>{output.src}</td>
-				<td>{output.weight}</td>
-				<td>{output.min}</td>
-				<td>{output.max}</td>
-				<td>{output.val}</td>
-				</tr>
-				)
-			})
-		}
-	}
-
-// renderContent() {
-// return (
-// <div>
-// <div style={{ display: (this.state.controller[0].id !== null) ? "block" : "none"}}>
-// <p>Controller channel assignment and configuration</p>
-// <Table id='gamepad-controller' striped bordered hover size="sm">
-// <thead>
-// <tr><th>Channel</th><th>Name</th><th>Source</th><th>Weight</th><th>Min</th><th>Max</th><th>Value</th></tr>
-// {this.renderControllerChannelData(this.state.controller)}
-// </thead>
-// <tbody>
-// </tbody>
-// </Table>
-// </div>
-// <div style={{ display: (this.state.controller[0].id === null) ? "block" : "none"}}>
-// <p>Please move an axes on the controller, to connect.</p>
-// </div>
-// </div>
-// )
-// }
-
-	renderContent() {
-		return (
-			<div>
-			</div>
-		)
-	}
+  renderContent() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
